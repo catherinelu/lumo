b8c32f471c21029ca4dd14b5a67b208de31e5e9a
huiminli <hmli@cs.stanford.edu>
2014-04-26 13:22:39 -0700

    added apps to settings.py

--- lumoproject/settings.py
+++ lumoproject/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gcal',
+    'lumoapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -67,7 +69,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Dawson'
 
 USE_I18N = True
 
