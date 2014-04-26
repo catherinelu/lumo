f4fa019436cd07dfc81441f3c531526261a8a9de
Catherine Lu <catherineglu@gmail.com>
2014-04-26 12:53:33 -0700

    Add home template

--- lumoproject/settings.py
+++ lumoproject/settings.py
@@ -26,6 +26,12 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = (
+  os.path.join(BASE_DIR, 'lumoapp/templates'),
+  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+)
 
 # Application definition
 
@@ -82,7 +88,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
 
 # The following was added by following directions from "Getting Started With Django"
 # via the Heroku website:
