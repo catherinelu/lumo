841fedb746cc1cb0ba5666150919927b9f4396a6
huiminli <hmli@cs.stanford.edu>
2014-04-26 13:47:51 -0700

    added views for gcal

--- lumoproject/urls.py
+++ lumoproject/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lumoapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^gcal/', include('gcal.urls', namespace='gcal')),
     url(r'^admin/', include(admin.site.urls)),
 )
