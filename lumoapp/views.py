c9ed931e5c2d922c1ce94ac63ccea3d64094d712
Catherine Lu <catherineglu@gmail.com>
2014-04-26 14:53:09 -0700

    Integrate Python Hue API

--- lumoapp/views.py
+++ lumoapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from hue import Hue
 
 def home(request):
+    h = Hue()
+    h.get_state()
+    light = h.lights.get('l1')
+    light.on()
+    light.bri(255)
+    light.off()
     return render(request, 'home.epy')
 
 
