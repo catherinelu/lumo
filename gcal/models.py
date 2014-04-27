4e1372f2c0fc5f89381045b8c6ac5d1aaeca70f4
Catherine Lu <catherineglu@gmail.com>
2014-04-27 03:20:12 -0700

    Add new alarm and bed event models

--- gcal/models.py
+++ gcal/models.py
@@ -1,13 +1,5 @@
 from django.db import models
-
-# Create your models here.
-
-import pickle
-import base64
-
-from django.contrib import admin
 from django.contrib.auth.models import User
-
 from oauth2client.django_orm import FlowField
 from oauth2client.django_orm import CredentialsField
 
--- lumoapp/models.py
+++ lumoapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AlarmEvent(models.Model):
+  user = models.ForeignKey(User)
+  alarm_time = models.DateTimeField()
+  notified = models.BooleanField(default=False)
+  should_notify = models.BooleanField(default=False)
+
+
+class BedEvent(models.Model):
+  user = models.ForeignKey(User)
+  bed_time = models.DateTimeField()
+  dim_time = models.IntegerField(default=10)
